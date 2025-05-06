097a6fc19fbc49df52d410e423fb6e37a60eaaa2
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-05-06 00:11:30 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
-function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+function DFS(graph, startnode, endnode) {
+    var Visited = new Array(graph.length).fill(0)
+    var ReturnList=[]
+    function DFS_(node) {
+        Visited[node] = 1;
+        if(Visited[endnode]==1){
+            ReturnList.push(node)
+            return 
+        }
+        for (const i of graph[node]) {
+            if (Visited[i] == 0 && Visited[endnode] == 0) {
+                DFS_(i)
+                if(Visited[endnode]==1){
+                    ReturnList.push(node)
+                    return
+                }
+            }
+
+        }
+    }
+    DFS_(startnode)
+    return ReturnList.reverse()
 }
