227e08b0d53b7d030b05c8b903edaf53f4992343
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-05-06 18:34:09 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
-
-var G3=[
+//Directed Graph
+var G=[
     [1,2,3],
     [0,4,5],
     [0,5],
@@ -11,21 +11,27 @@ var G3=[
     [1,2,3],
     [3]
 ]
-console.assert(JSON.stringify(DFS(G3,0,1))==JSON.stringify([0,1]))
-console.assert(JSON.stringify(DFS(G3,0,2))==JSON.stringify([0,1,5,2]))
-console.assert(JSON.stringify(DFS(G3,0,3))==JSON.stringify([0,1,5,3]))
-console.assert(JSON.stringify(DFS(G3,0,4))==JSON.stringify([0,1,4]))
-console.assert(JSON.stringify(DFS(G3,0,5))==JSON.stringify([0,1,5]))
-console.assert(JSON.stringify(DFS(G3,0,6))==JSON.stringify([0,1,5,3,6]))
-console.assert(JSON.stringify(DFS(G3,3,6))==JSON.stringify([3,6]))
-console.assert(JSON.stringify(DFS(G3,6,1))==JSON.stringify([6,3,0,1]))
-console.assert(JSON.stringify(DFS(G3,5,4))==JSON.stringify([5,1,4]))
-
+console.assert(JSON.stringify(DFS(G,0,1))==JSON.stringify([0,1]))
+console.assert(JSON.stringify(DFS(G,0,2))==JSON.stringify([0,1,5,2]))
+console.assert(JSON.stringify(DFS(G,0,3))==JSON.stringify([0,1,5,3]))
+console.assert(JSON.stringify(DFS(G,0,4))==JSON.stringify([0,1,4]))
+console.assert(JSON.stringify(DFS(G,0,5))==JSON.stringify([0,1,5]))
+console.assert(JSON.stringify(DFS(G,0,6))==JSON.stringify([0,1,5,3,6]))
+console.assert(JSON.stringify(DFS(G,3,6))==JSON.stringify([3,6]))
+console.assert(JSON.stringify(DFS(G,6,1))==JSON.stringify([6,3,0,1]))
+console.assert(JSON.stringify(DFS(G,5,4))==JSON.stringify([5,1,4]))
+//Undirected Graph
 G2=[
     [1,4],
-    [0,2,3],
-    [1],
-    [1],
-    [0,5,6],
+    [2,3],
+    [],
+    [],
+    [5,6],
+    [],
     []
 ]
+console.assert(JSON.stringify(DFS(G2,0,6))==JSON.stringify([0,4,6]))
+console.assert(JSON.stringify(DFS(G2,0,5))==JSON.stringify([0,4,5]))
+console.assert(JSON.stringify(DFS(G2,1,5))==JSON.stringify([]))
+console.assert(JSON.stringify(DFS(G2,6,0))==JSON.stringify([]))
+console.assert(JSON.stringify(DFS(G2,1,3))==JSON.stringify([0,1,3])
